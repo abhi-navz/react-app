83bb99f965ef954e1261cbbaf420822b008f05ef
abhi-navz <abhinavg01441@gmail.com>
2023-09-14 13:04:01 +0530

    named import/export and default named import/export

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 
-import Tile from "./Tile";
+import Tile, {A,B} from "./Tile"; 
+// in named imports the name's can't be changed, while the default one can have any name
 
 function App() {
   return (
@@ -17,6 +18,8 @@ function App() {
       </h1>
       <h2>How are you doing?</h2>
       <Tile />
+      <A/>
+      <B/>
       <img
         style={{ width: "200px" }}
         src="https://images.pexels.com/photos/18026534/pexels-photo-18026534/free-photo-of-photo-of-a-dog-with-long-hair-sitting-by-a-plant.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2://www.vecteezy.com/photo/17323715-3d-render-adorable-close-up-of-a-american-eskimo-dog-holding-red-rose-in-mouth-on-white-background"
--- src/Tile.js
+++ src/Tile.js
@@ -6,4 +6,16 @@ function Tile(){
     )
   }
 
+
+export function A(){
+  return <>
+  <p>This is named export A</p>
+  </>
+}
+export function B(){
+  return <>
+  <p>This is named export B</p>
+  </>
+}
+
   export default Tile;
