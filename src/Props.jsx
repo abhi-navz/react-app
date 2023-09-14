7c231344565be944bf557e5e457cd94bf4834a3f
abhi-navz <abhinavg01441@gmail.com>
2023-09-14 14:37:14 +0530

    props through destructuring

--- src/Props.jsx
+++ src/Props.jsx
@@ -6,14 +6,16 @@
 //         </>
 //     )
 // }
-function Avatar ({src,width,height,children}){
+
+// can be done throught destructuring 
+// function Avatar ({src,width,height,children}){
    
-    return (
-        <>
-        <img src={src} width={width}height={height}/>
-        {children}
-        </>
-    )
-}
+//     return (
+//         <>
+//         <img src={src} width={width}height={height}/>
+//         {children}
+//         </>
+//     )
+// }
 
 export default Avatar;
