9b1db80db8c4dcffbe60ebe92057be733e4e8632
abhi-navz <abhinavg01441@gmail.com>
2023-09-14 15:26:07 +0530

    s

--- src/Props.jsx
+++ src/Props.jsx
@@ -1,11 +1,11 @@
-// function Avatar (props){
-//     console.log(props)
-//     return (
-//         <>
-//         <img src={props.src} width={props.height}height={props.height}/>
-//         </>
-//     )
-// }
+function Avatar (props){
+    console.log(props)
+    return (
+        <>
+        <img src={props.src} width={props.height}height={props.height}/>
+        </>
+    )
+}
 
 // can be done throught destructuring 
 // function Avatar ({src,width,height,children}){
