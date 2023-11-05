f8a2554f0fbcfe2677b3f8396326fa099e2effb6
abhi-navz <abhinavg01441@gmail.com>
2023-11-05 19:56:40 +0530

    implemnte the logic of the game.

--- 04_tic_tac_toe/src/App.jsx
+++ 04_tic_tac_toe/src/App.jsx
@@ -4,9 +4,10 @@ function App() {
   
 
   return (
-    <>
+    < >
+    <h2 style={{  backgroundColor:'blueviolet', textAlign:'center', color:'white', padding:'10px', borderRadius:'5px'}}>Tic Tac Toe Game</h2>
     <Board/>
-    7
+   
     
     </>
   )
--- 04_tic_tac_toe/src/components/Board.jsx
+++ 04_tic_tac_toe/src/components/Board.jsx
@@ -1,39 +1,79 @@
-import React, { useState } from 'react';
-import Square from './Sqaure';
+import React, { useState } from "react";
+import Square from "./Sqaure";
 
-const Board = ()=> {
-
-    const [state, setState] = useState(Array(9).fill(null))
-    const [isXTurn, setIsXTurn] = useState(true)
-    const handleClick = (index)=> {
-       const copyState = [...state]
-       copyState[index] = isXTurn? "X":"O";
-       setState(copyState);
-       setIsXTurn(!isXTurn);
-
+const Board = () => {
+    const nullArray = Array(9).fill(null)
+    
+  const [state, setState] = useState(nullArray);
+  const [isXTurn, setIsXTurn] = useState(true);
+  const handleClick = (index) => {
+    if(state[index] !== null){
+        return;
     }
-    return (
-    <div className='board-container'>
-        <h2>Tic Tac Toe Game</h2>
-        <div className='board-row'>
-            <Square onClick = {()=> handleClick(0)} value={state[0]}/>
-            <Square  onClick = {()=> handleClick(1)}value={state[1]}/>
-            <Square  onClick = {()=> handleClick(2)} value={state[2]}/>
-        </div>
-        <div className='board-row'>
-            <Square  onClick = {()=> handleClick(3)} value={state[3]}/>
-            <Square  onClick = {()=> handleClick(4)} value={state[4]}/>
-            <Square  onClick = {()=> handleClick(5)} value={state[5]}/>
-        </div>
-        <div className='board-row'>
-            <Square  onClick = {()=> handleClick(6)} value={state[6]}/>
-            <Square  onClick = {()=> handleClick(7)} value={state[7]}/>
-            <Square  onClick = {()=> handleClick(8)} value={state[8]}/>
-        </div>
-        
+    const copyState = [...state];
+    copyState[index] = isXTurn ? "X" : "O";
+    setState(copyState);
+    setIsXTurn(!isXTurn);
+  };
 
+  const checkWinner = () => {
+    const winnerLogic = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (let logic of winnerLogic) {
+      const [a, b, c] = logic;
+      if (state[a] !== null && state[a] === state[b] && state[a] === state[c]) {
+        return state[a];
+      }
+    }
+    return false;
+  };
+
+  const isWinner = checkWinner();
+
+  const handleReset = () => {
+    setState(nullArray);
+
+  }
+
+  return (
+    <div className="board-container">
+      
+      {isWinner ? (
+        <>
+        <h3 style={{backgroundColor:'red', padding:'10px', borderRadius:'5px'}}>{isWinner} won the game </h3>
+        <button onClick={()=> handleReset()}>Play Again</button>
+        </>
+      ) : (
+        <>
+        <h4>Player {isXTurn?'X':"O"} move</h4>
+          <div className="board-row">
+            <Square onClick={() => handleClick(0)} value={state[0]} />
+            <Square onClick={() => handleClick(1)} value={state[1]} />
+            <Square onClick={() => handleClick(2)} value={state[2]} />
+          </div>
+          <div className="board-row">
+            <Square onClick={() => handleClick(3)} value={state[3]} />
+            <Square onClick={() => handleClick(4)} value={state[4]} />
+            <Square onClick={() => handleClick(5)} value={state[5]} />
+          </div>
+          <div className="board-row">
+            <Square onClick={() => handleClick(6)} value={state[6]} />
+            <Square onClick={() => handleClick(7)} value={state[7]} />
+            <Square onClick={() => handleClick(8)} value={state[8]} />
+          </div>
+        </>
+      )}
     </div>
-    )
-}
+  );
+};
 
 export default Board;
